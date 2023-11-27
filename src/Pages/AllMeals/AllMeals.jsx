e5deed5f681b2cfff5dc4813afe032fbd1e2c426
rakiburrahman307 <rakiburrahman307@gmail.com>
2023-11-27 14:35:56 +0600

    impliment the pagination system

--- src/Pages/AllMeals/AllMeals.jsx
+++ src/Pages/AllMeals/AllMeals.jsx
@@ -1,20 +1,59 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import { useEffect, useState } from "react";
 
 const AllMeals = () => {
+    const [itemsPerPage, setItemsPerPage] = useState(5);
+    const [currentPage, setCurrentPage] = useState(0)
+    const [carts, setCarts] = useState([])
     const axiosPublic = useAxiosPublic();
+
+
+    useEffect(() => {
+        axiosPublic(`/all_meals/pagination?page=${currentPage}&size=${itemsPerPage}`)
+            .then(res => setCarts(res.data))
+    }, [currentPage, itemsPerPage, axiosPublic])
     // Fetch all meals using React Query
-    const { data: carts, isLoading, isError } = useQuery({
-        queryKey: ['all_data'],
+    // const { data: carts, isLoading, isError, refetch } = useQuery({
+    //     queryKey: ['all_data'],
+    //     queryFn: async () => {
+    //         const res = await axiosPublic(`/all_meals/pagination?page=${currentPage}&size=${itemsPerPage}`);
+    //         return res.data;
+    //     }
+    // });
+    const { data: totalCount = { count: 0 } } = useQuery({
+        queryKey: ['count'],
         queryFn: async () => {
-            const res = await axiosPublic("/all_meals");
+            const res = await axiosPublic("/all_meals_count");
             return res.data;
         }
     });
+
+    const { count } = totalCount;
+
+    const numberOfPages = Math.ceil(Number(count) / itemsPerPage);
+    const pages = [...Array(numberOfPages).keys()];
+
+    const handleSetPerPage = e => {
+        const number = parseInt(e.target.value);
+        setItemsPerPage(number);
+        setCurrentPage(1);
+    }
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    const handleNextPage = () => {
+        if (currentPage < pages.length - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
     return (
         <div>
-            {isLoading && <span className="loading ml-[700px] loading-dots loading-lg"></span>}
-            {isError && <p>Error fetching data</p>}
+
 
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -27,7 +66,7 @@ const AllMeals = () => {
                                 Meal Type
                             </th>
                             <th scope="col" className="px-6 py-3">
-                            Distributor Name
+                                Distributor Name
                             </th>
                             <th scope="col" className="px-6 py-3">
                                 Posted Time
@@ -63,7 +102,7 @@ const AllMeals = () => {
                                     {cart?.postTime}
                                 </td>
                                 <td className="px-6 py-4">
-                                    ${cart?.price}
+                                    $ {cart?.price}
                                 </td>
                                 <td className="px-6 py-4">
                                     <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
@@ -75,30 +114,27 @@ const AllMeals = () => {
 
                     </tbody>
                 </table>
-                <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4" aria-label="Table navigation">
-                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900 dark:text-white">1-10</span> of <span className="font-semibold text-gray-900 dark:text-white">1000</span></span>
+                <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4 py-8" aria-label="Table navigation">
+                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900 dark:text-white">{currentPage}</span> of <span className="font-semibold text-gray-900 dark:text-white">{count}</span></span>
                     <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-                        </li>
-                        <li>
-                            <a href="#" aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
-                        </li>
+                        <div className="join mb-5">
+                            <button onClick={handlePrevPage} className="join-item btn">«</button>
+                            {
+                                pages.map((page, idx) => <button
+                                    onClick={() => setCurrentPage(page)}
+                                    key={idx} className={currentPage === page ? 'btn-active join-item btn' : 'join-item btn'}>{page}
+                                </button>)
+                            }
+                            <button onClick={handleNextPage} className="join-item btn">»</button>
+                            <select value={itemsPerPage} onChange={handleSetPerPage} className="select select-bordered w-full max-w-xs">
+                                <option value='5'>5</option>
+                                <option value='10'>10</option>
+                                <option value='20'>20</option>
+                                <option value='50'>50</option>
+                                <option value='100'>100</option>
+
+                            </select>
+                        </div>
                     </ul>
                 </nav>
             </div>
