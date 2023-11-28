e86994d500dfddff34fa1d4ec59564694097d56e
rakiburrahman307 <rakiburrahman307@gmail.com>
2023-11-29 03:19:09 +0600

    comment sectiondone

--- src/Layout/Home/Detail.jsx
+++ src/Layout/Home/Detail.jsx
@@ -1,10 +1,13 @@
-import { SlLike } from "react-icons/sl";
+import { BiSolidLike } from "react-icons/bi";
+import { BiSolidDislike } from "react-icons/bi";
 import { useLoaderData } from "react-router-dom";
 import ReactStarsRating from 'react-awesome-stars-rating';
 import useAuth from "../../Hooks/useAuth";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { toast } from "react-toastify";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
 
 const Detail = () => {
     const cardData = useLoaderData();
@@ -14,6 +17,8 @@ const Detail = () => {
     const customerEmail = user?.email;
     const status = 'pending';
     const { _id, mealType, mealImage, adminName, rating, likes, price, mealDescription, ingredients, postTime } = cardData;
+    const [liked, setLiked] = useState(false);
+    const [likeCount, setLikeCount] = useState(likes);
     const mealInfo = { customerName, customerEmail, mealType, mealImage, adminName, rating, likes, price, mealDescription, ingredients, postTime, status }
     const { data: planConfirm = [] } = useQuery({
         queryKey: ['allData'],
@@ -21,7 +26,23 @@ const Detail = () => {
             const res = await axiosSecure.get(`/plansConfirm/${customerEmail}`);
             return res.data;
         }
-    })
+    });
+    const { data: reviewsConfirm = [] } = useQuery({
+        queryKey: ['reviewsConfirmData'],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/all_meals/${_id}/reviewConfirm/${customerEmail}`);
+            return res.data;
+        }
+    });
+    const { data: reviews = [] } = useQuery({
+        queryKey: ['reviewsData'],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/all_meals/${_id}/reviews`);
+            return res.data;
+        }
+    });
+
+
     const handleRequestMeal = () => {
         if (!planConfirm) {
             return toast.error('Must Have Perches a Plan First')
@@ -29,7 +50,7 @@ const Detail = () => {
             axiosSecure.post('/req_meal', mealInfo)
                 .then(res => {
                     if (res?.data?.insertedId) {
-                        toast.success('Your request successfully Send');
+                        toast.success('Your request successfully send');
                     }
                 })
                 .catch(err => console.log(err.message));
@@ -40,7 +61,10 @@ const Detail = () => {
         const text = e.target.textarea.value;
         const comment = {customerName, customerEmail, text }
 
-        axiosSecure.post(`/all_meals_review/${_id}`, comment)
+        if (reviewsConfirm) {
+            return toast.warning('You Have Already Comment')
+        }else{
+            axiosSecure.post(`/all_meals_review/${_id}`, comment)
             .then(() => {
                 toast.success('Comment Added successfully');
             })
@@ -48,8 +72,22 @@ const Detail = () => {
                 console.error(err);
                 toast.error('Failed to add comment');
             });
+        }
 
     }
+    const handleLike = async () => {
+        setLiked((prevLiked) => !prevLiked);
+        setLikeCount((prevCount) => (liked ? prevCount - 1 : prevCount + 1));
+        try {
+            const res = await axiosSecure.post(`/all_meals/${_id}/like`);
+            if (res.data) {
+                toast.success('Liked')
+            }
+
+        } catch (error) {
+            console.error('Error updating like count:', error);
+        }
+    };
     return (
         <div>
             <div className="card card-side bg-base-100 shadow-xl">
@@ -66,7 +104,7 @@ const Detail = () => {
                     </div>
 
                     <div className="flex items-center">
-                        <button className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3 flex gap-2"><SlLike /> {likes}</button>
+                        <button onClick={handleLike} className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3 flex gap-2">{liked ? <BiSolidDislike /> : <BiSolidLike />} {likeCount}</button>
                     </div>
                     <p className="text-lg">PostTime: {postTime}</p>
                     <div className="items-center mb-5">
@@ -90,7 +128,17 @@ const Detail = () => {
                 </div>
             </div>
             <div className="py-10">
-                    <h2 className="text-2xl font-bold text-center">Customer Comment</h2>
+                    <h2 className="text-3xl font-bold text-center py-5">Customer Comment</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mx-5">
+                        {
+                            reviews.map((review, idx) =>
+                            <div key={idx} className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{review?.customerName}</h5>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">{review?.text}</p>
+                            </div>
+                            )
+                        }
+                    </div>
                 </div>
         </div>
     );
